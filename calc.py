621bd20db32b786fc89ce40fd98f0631863bfe6c
alexmitroff <alexmitroff@gmail.com>
2019-06-24 09:37:33 +0300

    Update calc script

--- calc.py
+++ calc.py
@@ -1,15 +1,17 @@
-from os import listdir
-from os.path import isfile, join
-import pandas as pd
-
 from engine.datahandler import DataHandler
 
+
 def main():
+    print('Calc starts')
     filename = "output.csv"
     handler = DataHandler('./source/exp2')
-    handler.save_stimuli_order(filename)
-    handler.transform_exp_data(filename)
-    handler.aggregate_exp_data(filename)
+    handler.save_stimuli_duration(filename)
+    # handler.save_stimuli_order(filename)
+    # handler.transform_exp_data(filename)
+    # handler.aggregate_exp_data(filename)
+    # handler.calc_pvalue('agg_count.csv')
+    print('Calc done')
+
 
 if __name__ == "__main__":
     main()
